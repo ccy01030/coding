349f6c6e6a25b1b797ecf4b5df8d66cd3ed39d08
ccy01030 <42595416+ccy01030@users.noreply.github.com>
2018-11-28 11:32:37 +0900

    Update Vector Test6.cpp

--- Vector Test6.cpp
+++ Vector Test6.cpp
@@ -6,6 +6,7 @@ class StoreVector
 private:
 	static const int row = 50;
 	static const int column = 50;
+	std::vector <std::vector<int>> v;
 
 public:
 	StoreVector(int arr[row][column]);
@@ -13,18 +14,15 @@ public:
 
 StoreVector::StoreVector(int arr[row][column])
 {
-	std::vector <std::vector<int>> v(100);
-
+	v.resize(100);
 	for (int i = 0; i < row; i++)
 	{
 		for (int j = 0; j < column; j++)
 		{
 			v[i].push_back(arr[i][j]);
-			std::cout << v[i][j];
 		}
 	}
 };
-
 
 int main()
 {
